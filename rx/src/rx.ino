09c8deced81d043f146494a9fea6584d4bd5a8b7
Glyn Hudson <glyn.hudson@gmail.com>
2017-01-04 21:14:23 +0000

    print something at startup

--- rx/src/rx.ino
+++ rx/src/rx.ino
@@ -115,7 +115,8 @@ void setup()
   //---------------------------------------------------------
 
     digitalWrite(LED_PIN, HIGH);
-//    Serial.println("Bee Monitor Rx V2.5 11Jul16");
+//   Serial.println("Bee Monitor Rx V2.5 11Jul16");
+    Serial.println("11 99 99 99 99");
     delay(1000);
     digitalWrite(LED_PIN, LOW);
    
