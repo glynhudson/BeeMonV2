afbfcef989d38872b29a06241cc2d9d4d9c63102
Glyn Hudson <glyn.hudson@gmail.com>
2017-01-04 21:30:52 +0000

    print correct number of integers

--- rx/src/rx.ino
+++ rx/src/rx.ino
@@ -116,7 +116,7 @@ void setup()
 
     digitalWrite(LED_PIN, HIGH);
 //   Serial.println("Bee Monitor Rx V2.5 11Jul16");
-    Serial.println("11 99 99 99 99");
+    Serial.println("11 9999 9999 9999 9999 9999");
     delay(1000);
     digitalWrite(LED_PIN, LOW);
    
